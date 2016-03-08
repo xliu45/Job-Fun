d6c97e9a619289368df0bb4bb9c1baa3522b472d
JL <fdiarla@gmail.com>
2016-03-08 16:57:36 -0600

    Create 100_Same_Tree.java

---
+++ LeetCode/Java/100_Same_Tree.java
@@ -0,0 +1,31 @@
+/*
+Given two binary trees, write a function to check if they are equal or not.
+
+Jingshen
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(q==null^p==null)
+            return false;
+        else if(p==null&&q==null)
+            return true;
+            else
+            {
+                if(p.val!=q.val)
+                    return false;
+                else
+                    return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+            }
+    }
+}
