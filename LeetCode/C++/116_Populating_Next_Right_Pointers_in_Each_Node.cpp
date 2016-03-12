9c504e4a618b66c7eb76da51ee3ff78d8c4401a6
xliu45 <amyleeathust@gmail.com>
2016-03-11 22:03:17 -0600

    Update and rename  116_Populating_Next_Right_Pointers_in_Each_Node.cpp to 116_Populating_Next_Right_Pointers_in_Each_Node.cpp

--- LeetCode/C++/116_Populating_Next_Right_Pointers_in_Each_Node.cpp
+++ LeetCode/C++/116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -1,15 +1,17 @@
 // 116. Populating Next Right Pointers in Each Node
 /*
 Given a binary tree
-
     struct TreeLinkNode {
       TreeLinkNode *left;
       TreeLinkNode *right;
       TreeLinkNode *next;
     }
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
-
 Initially, all next pointers are set to NULL.
+
+Tag: Tree, Depth-first Search
+
+Author: Xinyu Liu
 */
 
 #include <iostream>
