c724aa3bb50aca4cd4d80e7731f31b50a4ca0e0c
markjunior <markasjunior@gmail.com>
2016-03-16 01:49:55 -0500

    Added files via upload

---
+++ LeetCode/Java/337_House_Robber_III.java
@@ -0,0 +1,35 @@
+/*
+337. House Robber III
+
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int rob(TreeNode root) {
+        return dfs(root)[0];
+    }
+    
+    private int[] dfs(TreeNode root) {
+        int dp[]={0,0};
+        if(root != null){
+            int[] dp_L = dfs(root.left);
+            int[] dp_R = dfs(root.right);
+            dp[1] = dp_L[0] + dp_R[0];
+            dp[0] = Math.max(dp[1] ,dp_L[1] + dp_R[1] + root.val);
+        }
+        return dp;
+    }
+    
+}
