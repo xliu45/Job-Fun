6f01f73499cc57e23add4947888cad1d12287cc3
xliu45 <amyleeathust@gmail.com>
2016-03-19 18:32:00 -0500

    Create 230_Kth_Smallest_Element_in_a_BST.cpp

---
+++ LeetCode/C++/230_Kth_Smallest_Element_in_a_BST.cpp
@@ -0,0 +1,45 @@
+//230. Kth Smallest Element in a BST
+/*
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> nlist;
+        nodelist(root, nlist);
+        return nlist.at(k - 1);
+
+    }
+    void nodelist(TreeNode* root, vector<int> & nlist){
+        if(!root) return;
+        nodelist(root->left, nlist);
+        nlist.push_back(root->val);
+        nodelist(root->right, nlist);
+    }
+};
+
+void main(){
+    TreeNode n1(2),n2(1),n3(3);
+    n1.left = &n2;
+    n1.right = &n3;
+    Solution sol;
+    int kth = sol.kthSmallest(&n1,3);
+}
