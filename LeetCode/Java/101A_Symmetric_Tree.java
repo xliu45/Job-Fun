12a0736091814d302acf297b4d3f3e8210ca31c9
markjunior <markasjunior@gmail.com>
2016-03-21 03:14:14 -0500

    Added files via upload

---
+++ LeetCode/Java/101A_Symmetric_Tree.java
@@ -0,0 +1,42 @@
+/*
+101. Symmetric Tree
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
+            return true;
+        }
+        else{
+            return checkSym(root.left, root.right);
+        }
+    }
+    public boolean checkSym(TreeNode a, TreeNode b){
+        if (a==null && b==null){
+            return true;
+        }else{
+            if (a==null || b==null){
+                return false;
+            }else{
+                if (a.val==b.val){
+                    return checkSym(a.left,b.right) && checkSym(a.right,b.left)  ;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+    }
+}
---
+++ LeetCode/Java/101B_Symmetric_Tree.java
@@ -0,0 +1,54 @@
+/*
+101. Symmetric Tree
+
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        List<TreeNode> a = new ArrayList<TreeNode>();
+        List<TreeNode> b = new ArrayList<TreeNode>();
+        
+        a.add(root.left);
+        b.add(root.right);
+        
+        while(a.size()!=0 && b.size()!=0){
+            TreeNode l = a.remove(0);
+            TreeNode r = b.remove(0);
+            if (l==null && r==null){
+                continue;
+            }else{
+                if(l==null || r==null){
+                    return false;
+                }else{
+                    if(l.val==r.val){
+                        a.add(l.left);
+                        b.add(r.right);
+                        a.add(l.right);
+                        b.add(r.left);
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        if (a.size()==0 && b.size()==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
